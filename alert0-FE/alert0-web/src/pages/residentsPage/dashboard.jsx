c2cdf810c7f23c521280434810bacab45353afd0
Aji3-svg <ajichanthree@gmail.com>
2025-03-15 00:22:06 +0800

    updated dashboard responsive with UI option on lg sakit ngid likod kas bawi ko bwas mwaa

--- alert0-FE/alert0-web/src/pages/residentsPage/dashboard.jsx
+++ alert0-FE/alert0-web/src/pages/residentsPage/dashboard.jsx
@@ -1,12 +1,27 @@
 import Header from "../../components/headers/header";
-
-
+import Card from "../../components/card/card";
 const Dashboard = () => {
 
   return (
     <>
-      <Header/>
+    <header  className="flex justify-between bg-sky-950 items-center w-screen h-14 p-1 fixed top-0 left-0 uppercase text-white z-50">
+    <Header/>
+       </header>
+        <main className="pt-14 md:pt-50 lg:pt-0 border">
+        <div className= 'grid grid-cols-1 md:grid-cols-2' >
+        <Card
+        image='/images/test.jpg'
+        title='Ambulance'
+        buttonText='Request' />
+        <Card
+        image='/images/test.jpg'
+        title='Firetruck'
+        buttonText='Request' />
+    </div>
+        </main>
+       
       </>
+      
   )
 };
 
