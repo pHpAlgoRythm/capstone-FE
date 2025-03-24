213b9fb0a38845e3b646f725526c6de70c1835eb
Aji3-svg <ajichanthree@gmail.com>
2025-03-24 21:22:36 +0800

    Updated modified loginAPI

--- alert0-FE/alert0-web/src/services/API/loginAPI.jsx
+++ alert0-FE/alert0-web/src/services/API/loginAPI.jsx
@@ -2,13 +2,13 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 const LoginAuth = () => {
-
-
+  
   const navigate = useNavigate();
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const submitData = async (data) => {
     setIsSubmitting(true);
+    try {
     const response = await fetch("http://127.0.0.1:8000/api/login", {
       method: "POST",
       headers: {
@@ -16,23 +16,26 @@ const LoginAuth = () => {
       },
       body: JSON.stringify(data),
     });
-    try {
+    
+      if(!response.ok){
+        throw new Error('Invalid Credentials')
+      }
+
       const result = await response.json();
-      if (response.ok) {
+
         localStorage.setItem("token", result.data.token);
         localStorage.setItem("userID", result.data.id);
         localStorage.setItem("fullName", result.data.message);
-
+        
         if (result.data.role == "resident") {
           navigate("/resident/dashboard");
         } else if (result.data.role == "admin") {
           navigate("/adminPage/dashboard");
         } else alert("Invalid Account Log in");
-      } else {
-        throw new Error("Something went wrong");
-      }
+      
     } catch (error) {
-      console.log(error);
+      console.log(error.mesg);
+      alert(error.message)
     } finally {
       setIsSubmitting(false);
     }
